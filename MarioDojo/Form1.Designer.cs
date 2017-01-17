6e0fabdb259672f1262d19518c4d5bca8b4eb4fa
admin <admin@FATZ>
2017-01-17 12:23:16 +0100

    Add project files.

---
+++ MarioDojo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MarioDojo
+{
+    partial class MarioDojo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(1465, 806);
+            this.screen.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(663, 62);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 25);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MarioDojo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1465, 806);
+            this.Controls.Add(this.screen);
+            this.Name = "MarioDojo";
+            this.Text = "MarioDojo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MarioDojo_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MarioDojo_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MarioDojo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarioDojo
+{
+    public partial class MarioDojo : Form
+    {
+        bool right, left;
+        bool jump;
+
+        int G = 30; //G pour graviter
+        int Force; //force de la graviter
+
+        public MarioDojo()
+        {
+            InitializeComponent();
+        }
+
+        //Action part ------------------------------------------------------//
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(right == true) { player.Left += 5; }
+            if (left == true) { player.Left -= 5; }
+
+            if(jump == true)
+            {
+                //falling if the player has jumped before //Marche pas :(
+                player.Top -= Force;
+                Force -= 1;
+            }
+            if (player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height; //Stop falling at bottom
+            }
+            else
+            {
+                player.Top += 5; //falling
+            }
+
+           
+        }
+
+        /// Logique part --------------------------------------------------//
+        private void MarioDojo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = true; }
+            if(e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //Escape -> Exit
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = G;
+                }
+            }
+        }
+
+        private void MarioDojo_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = false; }
+            if(e.KeyCode == Keys.Left ) { left = false; }
+        }
+    }
+}
